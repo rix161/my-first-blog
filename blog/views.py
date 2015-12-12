b384ed22dc7a7a271ab515f6afdacde00a025f83
rohitrao666 <rohitrao666@gmail.com>
2015-12-12 22:51:00 +0530

    template

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 def post_list(request):
-	return render(request,'blog/post_list.html',{})
+	post = Post.objects.all().order_by("-pub_Date")
+	return render(request,'blog/post_list.html',{'posts':post})
 
